d57e75c234e8accbb51fedbc423e4467635754f7
chlgusrb97 <ikolp34@gmail.com>
2023-01-09 18:38:35 +0900

    프로젝트 저장하기

---
+++ freeboard_frontend/pages/01-project/index.js
@@ -0,0 +1,14 @@
+import {Body, Wrapper, HeadTitle} from '../../styles/01-project-style'
+
+export default function 게시물등록(){
+  
+  return (
+    <Body>
+      <Wrapper>
+        <HeadTitle>게시물 등록 </HeadTitle>
+      </Wrapper>
+    </Body>
+    
+  )
+
+}
---
+++ freeboard_frontend/styles/01-project-style.js
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled'
+
+
+export const Body = styled.body`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 1200px;
+  padding: 60px 0 100px;
+  background: blue;
+`
+
+export const HeadTitle = styled.h1`
+  font-size: 36px;
+  font-weight: 700;
+  margin-bottom: 80px;
+`
