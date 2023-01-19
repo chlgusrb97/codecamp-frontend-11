4b5f8f39344b3b2b7aee9263ff32c04440723639
chlgusrb97 <ikolp34@gmail.com>
2023-01-19 18:22:46 +0900

    Save Class

--- class/src/components/units/board/09-write2/BoardWrite.container.js
+++ class/src/components/units/board/09-write2/BoardWrite.container.js
@@ -1,7 +1,8 @@
 import { useMutation } from '@apollo/client'
 import { useState } from 'react'
-import { 나의그래프큐엘셋팅 } from './BoardWrite.queries'    // export는 골라서 가져오기 가능
+import { 나의그래프큐엘셋팅, UPDATE_BOARD } from './BoardWrite.queries'    // export는 골라서 가져오기 가능
 import BoardWriteUI from './BoardWrite.presenter'    // export default로 한개만 가져오기
+import { useRouter } from 'next/router'
 // import Sfdsfkjsdfjksdf from './BoardWrite.presenter'     // export default로 이름 바꿔서 가져오기
 // import Sfdsfkjsdfjksdf, { apple } from './BoardWrite.presenter'     // export default와 export를 함께 가져오기
 
@@ -11,58 +12,76 @@ import BoardWriteUI from './BoardWrite.presenter'    // export default로 한개
 
 
 export default function BoardWrite(props) {
-  const [isActive, setIsActive] = useState(false)
 
-  const [writer, setWriter] = useState()
-  const [title, setTitle] = useState()
-  const [contents, setContents] = useState()
+  const router = useRouter()
+
+  const [writer, setWriter] = useState("")
+  const [title, setTitle] = useState("")
+  const [contents, setContents] = useState("")
 
   const [나의함수] = useMutation(나의그래프큐엘셋팅)
+  const [updateBoard] = useMutation(UPDATE_BOARD)
 
   const onClickSubmit = async () => {
-    const result = await 나의함수({
-      variables: {                  // variables 이게 $ 역할을 함
-        writer,
-        title,
-        contents
-      }
+    
+    try {
+      const result = await 나의함수({
+        variables: {                  // variables 이게 $ 역할을 함
+          writer: writer,
+          title: title,
+          contents: contents
+        }
+      })
+      console.log(result)
+      router.push(`/section09/09-04-boards/${result.data.createBoard.number}`)
+    } catch(error) {
+      console.log(error)
+    }
+    
+  }
+
+  const onClickUpdate = async () => {
+    const myvariables = {number : Number(router.query.number)}
+    if(writer) myvariables.writer = writer
+    if(title) myvariables.title = title
+    if(contents) myvariables.contents = contents
+
+    // 여기서 수정하기
+    try {
+      const result = await updateBoard({
+      variables: myvariables
+      
     })
-    console.log(result)
+    console.log(result);
+    router.push(`/section09/09-04-boards/${result.data.updateBoard.number}`)
+    } catch(error) {
+      console.log(error)
+    }
   }
 
   const onChangeWriter = (event) => {
     setWriter(event.target.value)
-
-    if(event.target.value && title && contents) {
-      setIsActive(true)
-    } 
   }
 
   const onChangeTitle = (event) => {
     setTitle(event.target.value)
-    
-    if(writer && event.target.value && contents) {
-      setIsActive(true)
-    }
   }
 
   const onChangeContents = (event) => {
     setContents(event.target.value)
-
-    if(writer && title && event.target.value) {
-      setIsActive(true)
-    }
   }
 
   return (
     <div>
       <div>$$$$$$$$$$ 여기는 컨테이너입니다. $$$$$$$$$$</div>
       <BoardWriteUI
-      onClickSubmit={onClickSubmit} 
+      onClickSubmit={onClickSubmit}
+      onClickUpdate={onClickUpdate}
       onChangeWriter={onChangeWriter} 
       onChangeTitle={onChangeTitle} 
       onChangeContents={onChangeContents}
-      isActive={isActive}
+      isEdit={props.isEdit}
+      data={props.data} // undefined 이거나, data 이거나 둘 중 하나!
       />
       <div>$$$$$$$$$$ 여기는 컨테이너입니다. $$$$$$$$$$</div>
     </div>
