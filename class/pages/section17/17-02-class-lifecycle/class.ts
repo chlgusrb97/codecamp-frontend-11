ad5e5a37f2d3441acb33267c86f78addf9562b2d
chlgusrb97 <ikolp34@gmail.com>
2023-02-01 18:53:11 +0900

    Save Class

---
+++ class/pages/section17/17-02-class-lifecycle/class.ts
@@ -0,0 +1,44 @@
+// class Date {
+//   qqq = 3
+
+//   getFullYear() {
+
+//   }
+
+//   getMonth() {
+
+//   }
+// }
+
+// const date = new Date()
+// console.log(date.getFullYear)
+// console.log(date.getmMonth() + 1)
+
+// class Monster {
+//   power = 50
+
+//   attack() {
+//     console.log("공격합니다!!")
+//   }
+// }
+
+// // extends 기능 상속
+// class 슈퍼몬스터 extends Monster {
+//   run() {
+//     console.log("도망가자!!");
+//   }
+
+//   // 오버라이딩
+//   attack() {
+//     console.log("슈퍼몬스터 필살기!!")
+//   }
+// }
+
+// const monster = new Monster()
+// monster.power() // 50
+// monster.attack() // 공격합니f다!!
+
+// const myMonster = new 슈퍼몬스터()
+// myMonster.power() // 50
+// myMonster.attack() // 슈퍼몬스터필살기!!
+// myMonster.run() // 도망가자!!
---
+++ class/pages/section17/17-03-functional-lifecycle/index.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+export default function functionalCounterPage(): JSX.Element {
+  const [count, setCount] = useState(0);
+  const router = useRouter();
+
+  //  componentDidMount 와 동일
+  useEffect(() => {
+    console.log("그려지고 나서 실행!!");
+  }, [count]);
+
+  // componentDidMount + componentDidUpdate 와 동일
+  useEffect(() => {
+    console.log("변경되고 나서 실행!!");
+  });
+
+  useEffect(() => {
+    //  componentWillUnmount 와 동일
+    return () => {
+      console.log("사라지기 전에 실행!!");
+    };
+  }, []);
+
+  // 1. useEffect 하나로 합치기
+  useEffect(() => {
+    console.log("그려지고 나서 실행!!");
+
+    return () => {
+      console.log("사라지기 전에 실행!!");
+    };
+  }, []);
+
+  // 2. useEffect 잘못된 사용법 (1. 추가렌더링, 무한루프)
+  useEffect(() => {
+    setCount(1);
+  }, [count]);
+
+  const onClickCountUp = (): void => {
+    console.log(count);
+    setCount(1);
+  };
+
+  const onClickMove = (): void => {
+    void router.push("/");
+  };
+
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={onClickCountUp}>카운트 올리기!!</button>
+      <button onClick={onClickMove}>나가기!</button>
+    </>
+  );
+}
