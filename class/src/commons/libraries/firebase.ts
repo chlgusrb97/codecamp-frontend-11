cebb045dfdcca6eb1282f87725ac087098c10ad9
chlgusrb97 <ikolp34@gmail.com>
2023-02-03 15:03:38 +0900

    Class firebase

---
+++ class/src/commons/libraries/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVAhNItCicQI8rfV2b5y90YTN0zkOvkDE",
+  authDomain: "chk-firebase.firebaseapp.com",
+  projectId: "chk-firebase",
+  storageBucket: "chk-firebase.appspot.com",
+  messagingSenderId: "764856396425",
+  appId: "1:764856396425:web:08a8b2fb85ed493cb3f458",
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
