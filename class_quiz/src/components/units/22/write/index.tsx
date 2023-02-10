aa19883da7a5db5b0dd0a5f3bd0b9d94aeeb5e97
chlgusrb97 <ikolp34@gmail.com>
2023-02-10 16:36:55 +0900

    Class Quiz Recoil

---
+++ class_quiz/src/components/units/22/write/index.tsx
@@ -0,0 +1,11 @@
+import { useRecoilState } from "recoil";
+import { isEditState } from "../../../../commons/stores";
+
+export default function Write() {
+  const [isEdit, setIsEdit] = useRecoilState(isEditState);
+  return (
+    <>
+      <h1>{isEdit ? "수정하기" : "등록하기"}</h1>
+    </>
+  );
+}
