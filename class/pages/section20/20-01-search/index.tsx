be5c35fab98aea4bdea7274db04f80fc004b905b
chlgusrb97 <ikolp34@gmail.com>
2023-02-10 16:38:05 +0900

    Class Search

--- class/pages/section20/20-01-search/index.tsx
+++ class/pages/section20/20-01-search/index.tsx
@@ -1,10 +1,12 @@
+// 15-01
+
 import { useQuery, gql } from "@apollo/client";
-import { useState } from "react";
-import type { ChangeEvent, MouseEvent } from "react";
 import type {
   IQuery,
   IQueryFetchBoardsArgs,
 } from "../../../src/commons/types/generated/types";
+import type { ChangeEvent, MouseEvent } from "react";
+import { useState } from "react";
 
 const FETCH_BOARDS = gql`
   query fetchBoards($page: Int, $search: String) {
@@ -17,7 +19,7 @@ const FETCH_BOARDS = gql`
   }
 `;
 
-export default function StaticRountingMovedPage(): JSX.Element {
+export default function SearchBoard(): JSX.Element {
   const [search, setSearch] = useState("");
 
   const { data, refetch } = useQuery<
@@ -26,7 +28,8 @@ export default function StaticRountingMovedPage(): JSX.Element {
   >(FETCH_BOARDS);
 
   const onClickPage = (event: MouseEvent<HTMLSpanElement>): void => {
-    // 검색에서 refetch할 때, search 검색어가 refetch에 이미 저장되어 있는 상태이므로 추가로 search 포함하지 않아도 됨
+    // 검색에서 refetch할 때, search 검색어가 refetch에,
+    // 이미 저장되어 있는 상태이므로 추가로 search 포함하지 않아도 됨
     void refetch({ page: Number(event.currentTarget.id) });
   };
 
@@ -34,21 +37,27 @@ export default function StaticRountingMovedPage(): JSX.Element {
     setSearch(event.currentTarget.value);
   };
 
+  //  refetch : 그냥 refetch
+  //  refetchQuery : mutation 하고 refetch
   const onClickSearch = (): void => {
     void refetch({ search, page: 1 });
+    // 검색한 글을 1page에 보이게 합니다.
   };
+  // 현재는 제목을 검색할 수 밖에 없습니다. (API 기준 - 백엔드에 요청해야합니다)
 
   return (
     <div>
-      검색어입력: <input type="text" onChange={onChangeSearch} />
+      검색어 입력:
+      <input type="text" onChange={onChangeSearch} />
       <button onClick={onClickSearch}>검색하기</button>
       {data?.fetchBoards.map((el) => (
+        // fetchBoards에 타입을 주면 el은 자동으로 추론됩니다.
         <div key={el._id}>
           <span style={{ margin: "10px" }}>{el.title}</span>
           <span style={{ margin: "10px" }}>{el.writer}</span>
         </div>
       ))}
-      {new Array(10).fill("철수").map((_, index) => (
+      {new Array(10).fill(1).map((_, index) => (
         <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
           {index + 1}
         </span>
--- class/pages/section20/20-03-search-keyword/index.tsx
+++ class/pages/section20/20-03-search-keyword/index.tsx
@@ -1,12 +1,20 @@
+// 검색한 단어의 색상을 변경
+
+// 1. 키워드 중심으로 문장 쪼개는 방법 ex) 검색 : 점심 // 철수가, 점심, 을 먹었습니다.
+// 2. 키워드 저장할 state 만들기
+// 3. 분리된 문장 map으로 그리고, el 이 키워드와 같으면, 색 변경!
+
 import { useQuery, gql } from "@apollo/client";
-import { useState } from "react";
-import type { ChangeEvent, MouseEvent } from "react";
 import type {
   IQuery,
   IQueryFetchBoardsArgs,
 } from "../../../src/commons/types/generated/types";
+import type { ChangeEvent, MouseEvent } from "react";
+import { useState } from "react";
 import _ from "lodash";
 import { v4 as uuidv4 } from "uuid";
+// yarn add uuid
+// yarn add --dev @types/uuid
 
 const FETCH_BOARDS = gql`
   query fetchBoards($page: Int, $search: String) {
@@ -19,9 +27,8 @@ const FETCH_BOARDS = gql`
   }
 `;
 
-export default function StaticRountingMovedPage(): JSX.Element {
-  // const [search, setSearch] = useState("");
-  const [keyword, setKeyWord] = useState("");
+export default function SearchBoard(): JSX.Element {
+  const [keyword, setKeyword] = useState("");
 
   const { data, refetch } = useQuery<
     Pick<IQuery, "fetchBoards">,
@@ -29,29 +36,31 @@ export default function StaticRountingMovedPage(): JSX.Element {
   >(FETCH_BOARDS);
 
   const onClickPage = (event: MouseEvent<HTMLSpanElement>): void => {
-    // 검색에서 refetch할 때, search 검색어가 refetch에 이미 저장되어 있는 상태이므로 추가로 search 포함하지 않아도 됨
+    // 검색에서 refetch할 때, search 검색어가 refetch에,
+    // 이미 저장되어 있는 상태이므로 추가로 search 포함하지 않아도 됨
     void refetch({ page: Number(event.currentTarget.id) });
   };
 
   const getDebounce = _.debounce((value) => {
     void refetch({ search: value, page: 1 });
-    setKeyWord(value);
+    setKeyword(value);
   }, 500);
 
   const onChangeSearch = (event: ChangeEvent<HTMLInputElement>): void => {
-    // setSearch(event.currentTarget.value);
     getDebounce(event.currentTarget.value);
   };
 
-  // const onClickSearch = (): void => {
-  //   void refetch({ search, page: 1 });
-  // };
-
+  // 이렇게만 하면 입력 할때마다 refetch 되므로 비효율적임
+  // lodash 라이브러리 설치
+  // yarn add lodash
+  // yarn add --dev @types/lodash
   return (
     <div>
-      검색어입력: <input type="text" onChange={onChangeSearch} />
+      검색어 입력:
+      <input type="text" onChange={onChangeSearch} />
       {/* <button onClick={onClickSearch}>검색하기</button> */}
       {data?.fetchBoards.map((el) => (
+        // fetchBoards에 타입을 주면 el은 자동으로 추론됩니다.
         <div key={el._id}>
           <span style={{ margin: "10px" }}>
             {el.title
@@ -69,7 +78,7 @@ export default function StaticRountingMovedPage(): JSX.Element {
           <span style={{ margin: "10px" }}>{el.writer}</span>
         </div>
       ))}
-      {new Array(10).fill("철수").map((_, index) => (
+      {new Array(10).fill(1).map((_, index) => (
         <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
           {index + 1}
         </span>
