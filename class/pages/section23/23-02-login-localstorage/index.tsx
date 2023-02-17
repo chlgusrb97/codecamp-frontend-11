85103f27ca526f7d19047efb4cead6d2cc910e90
chlgusrb97 <ikolp34@gmail.com>
2023-02-17 22:04:30 +0900

    Class Login Localstorage

--- class/pages/section23/23-02-login-localstorage/index.tsx
+++ class/pages/section23/23-02-login-localstorage/index.tsx
@@ -41,30 +41,30 @@ export default function LoginPage(): JSX.Element {
     const result = localStorage.getItem("accessToken");
     console.log(result);
 
-    // try {
-    //   // 1. 로그인 뮤테이션 날려서 accessToken 받아오기
-    //   const result = await loginUser({
-    //     variables: {
-    //       email,
-    //       password,
-    //     },
-    //   });
-    //   const accessToken = result.data?.loginUser.accessToken;
-    //   console.log(accessToken);
+    try {
+      // 1. 로그인 뮤테이션 날려서 accessToken 받아오기
+      const result = await loginUser({
+        variables: {
+          email,
+          password,
+        },
+      });
+      const accessToken = result.data?.loginUser.accessToken;
+      console.log(accessToken);
 
-    //   // 2. 받아온 accessToken을 globalState에 저장하기
-    //   if (accessToken === undefined) {
-    //     alert("로그인에 실패했습니다! 다시 시도해 주세요!");
-    //     return;
-    //   }
-    //   setAcccessToken(accessToken);
-    //   localStorage.setItem("accessToken", accessToken); // 임시 사용(나중에 사용예정)
+      // 2. 받아온 accessToken을 globalState에 저장하기
+      if (accessToken === undefined) {
+        alert("로그인에 실패했습니다! 다시 시도해 주세요!");
+        return;
+      }
+      setAcccessToken(accessToken);
+      localStorage.setItem("accessToken", accessToken); // 임시 사용(나중에 사용예정)
 
-    //   // 3. 로그인 성공 페이지로 이동하기
-    //   void router.push("/section23/23-02-login-localstorage-success");
-    // } catch (error) {
-    //   if (error instanceof Error) alert(error.message);
-    // }
+      // 3. 로그인 성공 페이지로 이동하기
+      void router.push("/section23/23-02-login-localstorage-success");
+    } catch (error) {
+      if (error instanceof Error) alert(error.message);
+    }
   };
 
   return (
