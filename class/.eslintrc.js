428a4ee2d791cce4fac542dfd752a9ce5764d647
chlgusrb97 <ikolp34@gmail.com>
2023-03-08 23:45:41 +0900

    [2023.3.8] 배포

--- class/.eslintrc.js
+++ class/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
   overrides: [],
@@ -13,7 +14,6 @@ module.exports = {
   plugins: ["react"],
   rules: {
     "react/react-in-jsx-scope": "off",
-    // "@typescript-eslint/no-misused-promises": "off",
     "react/display-name": "off",
     "@typescript-eslint/naming-convention": "off",
     "@typescript-eslint/restrict-template-expressions": "off",
